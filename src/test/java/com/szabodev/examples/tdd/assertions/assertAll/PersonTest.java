5aff1cdb7fc0981cdc450c9658587d563f5aa79d
Zoltán Szabó <zoltan.szabo@atolldev.hu>
2019-10-17 08:28:50 +0200

    AssertJ and Hamcrest

--- src/test/java/com/szabodev/examples/tdd/assertions/assertAll/PersonTest.java
+++ src/test/java/com/szabodev/examples/tdd/assertions/assertAll/PersonTest.java
@@ -3,6 +3,8 @@ package com.szabodev.examples.tdd.assertions.assertAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.core.Is.is;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -22,5 +24,15 @@ class PersonTest {
                 () -> assertEquals("last", person.getLastName()),
                 () -> assertEquals("email", person.getEmail())
         );
+    }
+
+    @Test
+    void assertWithAssertJ() {
+        assertThat(person.getFirstName()).isEqualTo("first");
+    }
+
+    @Test
+    void assertWithHamcrest() {
+        org.hamcrest.MatcherAssert.assertThat(person.getLastName(), is("last"));
     }
 }
